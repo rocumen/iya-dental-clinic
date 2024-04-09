08ca161b3903eb084e370660fb29baa4e104342e
rocumen <rocumen28@gmail.com>
2024-04-09 22:40:10 +0800

    added confirmation
    confirmation when changing the status of the procedure in incomingPatients modal

--- frontend/src/components/IncomingAppointments.js
+++ frontend/src/components/IncomingAppointments.js
@@ -18,14 +18,17 @@ const IncomingAppointments = ({ patient }) => {
   });
 
   const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [confirmationModalOpen, setConfirmationModalOpen] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
   const [filteredData, setFilteredData] = useState([]);
+  const [loadingStates, setLoadingStates] = useState({});
+  const [selectedProcedureId, setSelectedProcedureId] = useState(null);
+  const [selectedPatientId, setSelectedPatientId] = useState(null);
 
   const [changeProcedureStatus] = useChangeProcedureStatusMutation();
 
-  const [loadingStates, setLoadingStates] = useState({});
-
   const handleStatusUpdate = async (procedureId, patientId) => {
+    setConfirmationModalOpen(false);
     setLoadingStates((prevLoadingStates) => ({
       ...prevLoadingStates,
       [procedureId]: true,
@@ -50,22 +53,16 @@ const IncomingAppointments = ({ patient }) => {
     }
   };
 
-  const openModal = () => {
-    setModalIsOpen(true);
+  const openConfirmationModal = (procedureId, patientId) => {
+    setSelectedProcedureId(procedureId);
+    setSelectedPatientId(patientId);
+    setConfirmationModalOpen(true);
   };
 
-  const closeModal = () => {
-    setModalIsOpen(false);
-  };
-
-  const formatDate = (dateString) => {
-    const options = {
-      weekday: "long",
-      month: "long",
-      day: "numeric",
-      year: "numeric",
-    };
-    return new Date(dateString).toLocaleDateString("en-US", options);
+  const closeConfirmationModal = () => {
+    setSelectedProcedureId(null);
+    setSelectedPatientId(null);
+    setConfirmationModalOpen(false);
   };
 
   const handleSearchChange = (event) => {
@@ -88,10 +85,15 @@ const IncomingAppointments = ({ patient }) => {
     setFilteredData(filtered);
   };
 
-  // if done = false
-  // if on going = true
-
-  //nextAppointment = true && status === true
+  const formatDate = (dateString) => {
+    const options = {
+      weekday: "long",
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+    };
+    return new Date(dateString).toLocaleDateString("en-US", options);
+  };
 
   const renderTableBody = () => {
     const displayData = filteredData.length ? filteredData : data;
@@ -106,24 +108,16 @@ const IncomingAppointments = ({ patient }) => {
             <td>
               <div className="text-center">
                 {" "}
-                {/* <Button
-                  size="sm"
-                  style={{
-                    backgroundColor: patient.status ? "blue" : "green",
-                  }}
-                  onClick={() =>
-                    handleStatusUpdate(patient.procedureId, patient.patientId)
-                  }
-                >
-                  {patient.status ? "On going" : "Done"}
-                </Button> */}
                 <Button
                   size="sm"
                   style={{
                     backgroundColor: patient.status ? "blue" : "green",
                   }}
                   onClick={() =>
-                    handleStatusUpdate(patient.procedureId, patient.patientId)
+                    openConfirmationModal(
+                      patient.procedureId,
+                      patient.patientId
+                    )
                   }
                   disabled={loadingStates[patient.procedureId]}
                 >
@@ -143,13 +137,16 @@ const IncomingAppointments = ({ patient }) => {
 
   return (
     <div>
-      {/* {isLoading && <Loader />} */}
       <div className="text-end">
-        <Button variant="primary" className="btn-md mx-1 " onClick={openModal}>
+        <Button
+          variant="primary"
+          className="btn-md mx-1 "
+          onClick={() => setModalIsOpen(true)}
+        >
           Appointments
         </Button>
       </div>
-      <Modal size="lg" show={modalIsOpen} onHide={closeModal}>
+      <Modal size="lg" show={modalIsOpen} onHide={() => setModalIsOpen(false)}>
         <Modal.Header>
           <Modal.Title>Incoming Appointments</Modal.Title>
           <Form.Control
@@ -172,7 +169,28 @@ const IncomingAppointments = ({ patient }) => {
           </Table>
         </Modal.Body>
         <Modal.Footer>
-          <Button onClick={closeModal}>Close</Button>
+          <Button onClick={() => setModalIsOpen(false)}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+
+      {/* Confirmation Modal */}
+      <Modal show={confirmationModalOpen} onHide={closeConfirmationModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Confirm Status Update</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Are you sure you want to update the status?</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={closeConfirmationModal}>
+            Cancel
+          </Button>
+          <Button
+            variant="primary"
+            onClick={() =>
+              handleStatusUpdate(selectedProcedureId, selectedPatientId)
+            }
+          >
+            Confirm
+          </Button>
         </Modal.Footer>
       </Modal>
     </div>
