5fd874a9f2b52354b7e9b1071e9bd218f2cc26fa
rocumen <rocumen28@gmail.com>
2024-05-10 01:15:36 +0800

    added

--- backend/controller/uploadController.js
+++ backend/controller/uploadController.js
@@ -154,6 +154,7 @@ const dataPrivacySignature = asyncHandler(async (req, res) => {
   }
 });
 
+/*
 const rx = async (req, res) => {
   try {
     const files = req.files; // Use req.files to get an array of uploaded files
@@ -186,53 +187,53 @@ const rx = async (req, res) => {
     res.status(500).send({ message: "Internal Server Error" });
   }
 };
+*/
+const rx = async (req, res) => {
+  try {
+    const files = req.files;
 
-// const rx = async (req, res) => {
-//   try {
-//     const files = req.files;
+    if (!files || files.length === 0) {
+      return res.status(400).send({ message: "No files uploaded" });
+    }
 
-//     if (!files || files.length === 0) {
-//       return res.status(400).send({ message: "No files uploaded" });
-//     }
+    const rxs = [];
 
-//     const rxs = [];
+    for (const file of files) {
+      // Use Sharp to resize the image
+      const resizedImageBuffer = await sharp(file.path)
+        .resize({ width: 451, height: 359 })
+        .toBuffer();
 
-//     for (const file of files) {
-//       // Use Sharp to resize the image
-//       const resizedImageBuffer = await sharp(file.path)
-//         .resize({ width: 451, height: 359 })
-//         .toBuffer();
+      // Upload the resized image to Cloudinary
+      const result = await new Promise((resolve, reject) => {
+        cloudinary.v2.uploader
+          .upload_stream({ resource_type: "image" }, (error, result) => {
+            if (error) {
+              console.error("Error uploading image to Cloudinary:", error);
+              reject(error);
+            } else {
+              resolve(result);
+            }
+          })
+          .end(resizedImageBuffer);
+      });
 
-//       // Upload the resized image to Cloudinary
-//       const result = await new Promise((resolve, reject) => {
-//         cloudinary.v2.uploader
-//           .upload_stream({ resource_type: "image" }, (error, result) => {
-//             if (error) {
-//               console.error("Error uploading image to Cloudinary:", error);
-//               reject(error);
-//             } else {
-//               resolve(result);
-//             }
-//           })
-//           .end(resizedImageBuffer);
-//       });
+      const rx = {
+        url: result.secure_url,
+        id: result.public_id,
+      };
 
-//       const rx = {
-//         url: result.secure_url,
-//         id: result.public_id,
-//       };
-
-//       rxs.push(rx);
-//     }
-//     res.status(200).send({
-//       message: "Images uploaded successfully",
-//       rxs: rxs,
-//     });
-//   } catch (error) {
-//     console.error("Error:", error);
-//     res.status(500).send({ message: "Internal Server Error" });
-//   }
-// };
+      rxs.push(rx);
+    }
+    res.status(200).send({
+      message: "Images uploaded successfully",
+      rxs: rxs,
+    });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send({ message: "Internal Server Error" });
+  }
+};
 // DATA PRIVACY SIGNATURE
 // @ /upload/dataPrivacySignature
 const procedureSignature = asyncHandler(async (req, res) => {
